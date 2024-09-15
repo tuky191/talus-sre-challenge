4632cf0a8d98373a4166bd04039cd518e1b73af3
tuky191 <michal@terra.money>
2024-09-15 09:34:26 +0200

    setting up terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -51,7 +51,7 @@ resource "google_compute_instance" "default" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
     }
   }
 
