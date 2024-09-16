feadaad887129f99c80d507c6beb98b9c0dcdf8a
tuky191 <michal@terra.money>
2024-09-16 13:25:21 +0200

    network policy update

--- infra/terraform/firewall.tf
+++ infra/terraform/firewall.tf
@@ -1,7 +1,6 @@
-# Block all egress from backend to monitoring
-resource "kubernetes_network_policy_v1" "deny_egress_to_monitoring" {
+resource "kubernetes_network_policy_v1" "allow_ingress_to_backend_on_5000" {
   metadata {
-    name      = "deny-egress-backend-to-monitoring"
+    name      = "allow-ingress-to-backend-5000"
     namespace = kubernetes_namespace.backend_namespace.metadata[0].name
   }
 
@@ -10,26 +9,28 @@ resource "kubernetes_network_policy_v1" "deny_egress_to_monitoring" {
       match_labels = {}
     }
 
-    policy_types = ["Egress"]
+    policy_types = ["Ingress"]
 
-    egress {
-      to {
-        namespace_selector {
-          match_labels = {
-            "app.kubernetes.io/name" = "monitoring"
-          }
+    ingress {
+      from {
+        ip_block {
+          cidr = "0.0.0.0/0"
         }
       }
-      # No port exceptions, blocking all egress to monitoring
+
+      ports {
+        port     = 5000
+        protocol = "TCP"
+      }
     }
   }
 }
 
-# Block egress from backend to data namespace except for Redis port (6379)
-resource "kubernetes_network_policy_v1" "deny_egress_to_data" {
+
+resource "kubernetes_network_policy_v1" "allow_ingress_to_data_on_6379" {
   metadata {
-    name      = "deny-egress-backend-to-data"
-    namespace = kubernetes_namespace.backend_namespace.metadata[0].name
+    name      = "allow-ingress-to-data-6379"
+    namespace = kubernetes_namespace.data_namespace.metadata[0].name
   }
 
   spec {
@@ -37,20 +38,73 @@ resource "kubernetes_network_policy_v1" "deny_egress_to_data" {
       match_labels = {}
     }
 
-    policy_types = ["Egress"]
+    policy_types = ["Ingress"]
 
-    egress {
-      to {
-        namespace_selector {
-          match_labels = {
-            "app.kubernetes.io/name" = "data"
-          }
+    ingress {
+      from {
+        ip_block {
+          cidr = "0.0.0.0/0"
         }
       }
 
-      # Allow egress only for Redis on port 6379
       ports {
         port     = 6379
+        protocol = "TCP"
+      }
+    }
+  }
+}
+
+
+resource "kubernetes_network_policy_v1" "allow_ingress_to_monitoring_on_3000_and_prometheus_ports" {
+  metadata {
+    name      = "allow-ingress-to-monitoring-3000-prometheus"
+    namespace = kubernetes_namespace.monitoring_namespace.metadata[0].name
+  }
+
+  spec {
+    pod_selector {
+      match_labels = {}
+    }
+
+    policy_types = ["Ingress"]
+
+    ingress {
+      from {
+        ip_block {
+          cidr = "0.0.0.0/0"
+        }
+      }
+
+      ports {
+        port     = 3000
+        protocol = "TCP"
+      }
+    }
+
+    ingress {
+      from {
+        ip_block {
+          cidr = "0.0.0.0/0"
+        }
+      }
+
+      ports {
+        port     = 9090
+        protocol = "TCP"
+      }
+    }
+
+
+    ingress {
+      from {
+        ip_block {
+          cidr = "0.0.0.0/0"
+        }
+      }
+
+      ports {
+        port     = 8080
         protocol = "TCP"
       }
     }
